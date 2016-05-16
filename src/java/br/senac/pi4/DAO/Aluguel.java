454f43e77a8df145ce9bf0eb9778b77fc086afcc
bruno.lopes <bruno.lopes@kron037.kronmed.com.br>
2016-05-16 16:50:45 -0300

    Versão Inicial. Foi adicionado a parte do ejb e bean inicial do Usuario Criação das pastas WEB: resources css fonts imagens js

---
+++ src/java/br/senac/pi4/DAO/Aluguel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.pi4.DAO;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bruno.lopes
+ */
+public class Aluguel {
+    
+    private int id;
+    private String localizacaoRetirada;
+    private Date dataRetirada;
+    private Date horaRetirada;
+    private String localizacaoDevolucao;
+    private Date dataDevolucao;
+    private Date horaDevolucao;
+    
+}
---
+++ src/java/br/senac/pi4/bean/AluguelBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.pi4.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author bruno.lopes
+ */
+@ManagedBean
+@SessionScoped
+public class AluguelBean {
+
+    /**
+     * Creates a new instance of AluguelBean
+     */
+    public AluguelBean() {
+    }
+    
+}
