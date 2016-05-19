e013459ba59a9505641a940bdccdb91f74137d37
Bruno Costa <b_c_l@DESKTOP-TNH7R1B>
2016-05-18 22:02:29 -0300

    Telas parcialmente em JSF

--- src/java/br/senac/pi4/DAO/Aluguel.java
+++ src/java/br/senac/pi4/DAO/Aluguel.java
@@ -16,9 +16,130 @@ public class Aluguel {
     private int id;
     private String localizacaoRetirada;
     private Date dataRetirada;
-    private Date horaRetirada;
+    private String horaRetirada;
     private String localizacaoDevolucao;
     private Date dataDevolucao;
-    private Date horaDevolucao;
-    
+    private String horaDevolucao;
+    private String grupo;
+    private boolean protecao;
+    private boolean bbConf;
+    private boolean assElev;
+    private boolean cadBB;
+    private boolean gps;
+    private String selectAcessorios;
+
+    public Aluguel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocalizacaoRetirada() {
+        return localizacaoRetirada;
+    }
+
+    public void setLocalizacaoRetirada(String localizacaoRetirada) {
+        this.localizacaoRetirada = localizacaoRetirada;
+    }
+
+    public Date getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(Date dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public String getHoraRetirada() {
+        return horaRetirada;
+    }
+
+    public void setHoraRetirada(String horaRetirada) {
+        this.horaRetirada = horaRetirada;
+    }
+
+    public String getLocalizacaoDevolucao() {
+        return localizacaoDevolucao;
+    }
+
+    public void setLocalizacaoDevolucao(String localizacaoDevolucao) {
+        this.localizacaoDevolucao = localizacaoDevolucao;
+    }
+
+    public Date getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(Date dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public String getHoraDevolucao() {
+        return horaDevolucao;
+    }
+
+    public void setHoraDevolucao(String horaDevolucao) {
+        this.horaDevolucao = horaDevolucao;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public boolean getProtecao() {
+        return protecao;
+    }
+
+    public void setProtecao(boolean protecao) {
+        this.protecao = protecao;
+    }
+
+    public boolean getBbConf() {
+        return bbConf;
+    }
+
+    public void setBbConf(boolean bbConf) {
+        this.bbConf = bbConf;
+    }
+
+    public boolean getAssElev() {
+        return assElev;
+    }
+
+    public void setAssElev(boolean assElev) {
+        this.assElev = assElev;
+    }
+
+    public boolean getCadBB() {
+        return cadBB;
+    }
+
+    public void setCadBB(boolean cadBB) {
+        this.cadBB = cadBB;
+    }
+
+    public boolean isGps() {
+        return gps;
+    }
+
+    public void setGps(boolean gps) {
+        this.gps = gps;
+    }
+
+    public String getSelectAcessorios() {
+        return selectAcessorios;
+    }
+
+    public void setSelectAcessorios(String selectAcessorios) {
+        this.selectAcessorios = selectAcessorios;
+    }
 }
--- src/java/br/senac/pi4/bean/AluguelBean.java
+++ src/java/br/senac/pi4/bean/AluguelBean.java
@@ -5,6 +5,7 @@
  */
 package br.senac.pi4.bean;
 
+import br.senac.pi4.DAO.Aluguel;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,11 +16,41 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class AluguelBean {
-
-    /**
-     * Creates a new instance of AluguelBean
-     */
+    
+    private Aluguel aluguel;
+    
     public AluguelBean() {
+        aluguel = new Aluguel();
+    }
+
+    public Aluguel getAluguel() {
+        return aluguel;
+    }
+
+    public void setAluguel(Aluguel aluguel) {
+        this.aluguel = aluguel;
+    }
+    
+    public String escolherGrupo(){
+       return "escolherGrupo";
+    }
+    
+    public String carroSelecionado(String grupo)
+    {
+       this.aluguel.setGrupo(grupo);
+       return "escolherGrupo.xhtml?faces-redirect=true";       
+    }
+    
+    public String protecao(){
+        return "protecao";
+    }
+    
+    public String localData(){
+        return "localData";
+    }
+    
+    public String resumo(){
+        return "resumo";
     }
     
 }
